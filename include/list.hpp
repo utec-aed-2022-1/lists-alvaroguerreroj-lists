d47bc7be43d8ae10b5d93261cd28830b94e43c13
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-04-07 01:25:10 -0500

    feat(list): Use `size_type` instead of int

--- include/list.hpp
+++ include/list.hpp
@@ -22,11 +22,11 @@ public:
     virtual void push_back(T) = 0;
     virtual T pop_front() = 0;
     virtual T pop_back() = 0;
-    virtual T insert(T, int) = 0;
-    virtual bool remove(int) = 0;
-    virtual T& operator[](int) = 0; // debe ser declarado en cada clase hija
+    virtual T insert(T, size_type) = 0;
+    virtual bool remove(size_type) = 0;
+    virtual T& operator[](size_type) = 0; // debe ser declarado en cada clase hija
     virtual bool is_empty() = 0;
-    virtual int size() = 0;
+    virtual size_type size() = 0;
     virtual void clear() = 0;
     virtual void sort() = 0;
     virtual bool is_sorted() = 0;
