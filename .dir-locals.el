0335110293530c9dec1c9993207308ebd1378c6e
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-04-14 19:13:52 -0500

    build(dir-locals): Add `fill-column` in `c++-mode`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((git-commit-summary-max-length . 72))))
+((nil . ((git-commit-summary-max-length . 72)))
+ (c++-mode . ((fill-column . 96))))
