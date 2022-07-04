80bbcb914b916c5f564a85831e941481b44b93a1
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-07-04 08:44:48 -0500

    build(nix/shell): Update

--- shell.nix
+++ shell.nix
@@ -1,24 +1,33 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (builtins.fetchTarball {
+  name = "nixos-clangd-working";
+  url =
+    "https://github.com/nixos/nixpkgs/archive/9e49886b3d83d18ca59f66ec7b388315fa3d2e60.tar.gz";
+  sha256 = "0bkdm4wf5vlg6mgv6r6672imx84axi5s159jd1f90gj5da2mzmfm";
+}) { } }:
 
 with pkgs;
 
-mkShell {
-  buildInputs = [
+mkShell rec {
+  buildInputs = [ llvmPackages_latest.libcxx ];
+
+  packages = [
     clang_14
     gcc
 
-    cling
-    clang-tools
-    valgrind
-    gdb
     cgdb
+    clang-tools
+    cling
+    gdb
+    valgrind
 
+    cmake
     just
-    gnumake
-    bear
     meson
     ninja
+    pkg-config
+
+    gnuplot
   ];
 
-  nativeBuildInputs = [ clang_14 ];
+  CPATH = lib.makeSearchPathOutput "dev" "include" buildInputs;
 }
